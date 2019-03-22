5d5c31031148d351c9def5e91a24d3804496adae
tcje91 <tcje9117@outlook.com>
2019-03-22 20:52:17 +0000

    fixed nodemon and urlparser error

--- server/app.js
+++ server/app.js
@@ -2,12 +2,15 @@ const app = require("express")();
 const graphqlHTTP = require("express-graphql");
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
-const { username, password } = require("./config")
+const { username, password } = require("./config");
 
-mongoose.connect(`mongodb+srv://${username}:${password}@graphql-test-filmdb-y3sp1.mongodb.net/test?retryWrites=true`);
+mongoose.connect(
+  `mongodb+srv://${username}:${password}@graphql-test-filmdb-y3sp1.mongodb.net/test?retryWrites=true`,
+  { useNewUrlParser: true }
+);
 mongoose.connection.once("open", () => {
-  console.log("connected to database")
-})
+  console.log("connected to database");
+});
 
 app.use(
   "/graphql",
