16331fc969e52494ac6628d6ff2e29da4db9158f
tcje91 <tcje9117@outlook.com>
2019-03-22 15:54:47 +0000

    add director type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,13 @@
 const graphql = require("graphql");
 const _ = require("lodash");
 
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLInt,
+  GraphQLSchema,
+  GraphQLID
+} = graphql;
 
 //dummy data
 const films = [
@@ -10,13 +16,28 @@ const films = [
   { film_id: "3", title: "The Big Lebowski", genre: "Comedy", year: 1998 }
 ];
 
+const directors = [
+  { director_id: "1", name: "Robert Rodriguez", age: 50 },
+  { director_id: "2", name: "Ridley Scott", age: 81 },
+  { director_id: "3", name: "Ethan Cohen", age: 61 }
+];
+
 const FilmType = new GraphQLObjectType({
   name: "Film",
   fields: () => ({
-    film_id: { type: GraphQLString },
+    film_id: { type: GraphQLID },
     title: { type: GraphQLString },
     genre: { type: GraphQLString },
     year: { type: GraphQLInt }
+  })
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    director_id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt }
   })
 });
 
@@ -25,10 +46,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     film: {
       type: FilmType,
-      args: { film_id: { type: GraphQLString } },
+      args: { film_id: { type: GraphQLID } },
       resolve(parent, args) {
         //code to fetch book from db
         return _.find(films, { film_id: args.film_id });
+      }
+    },
+    director: {
+      type: DirectorType,
+      args: { director_id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(directors, { director_id: args.director_id });
       }
     }
   }
