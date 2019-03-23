0b50bd72575c39c0b0939748d40b0775ba00e9cc
tcje91 <tcje9117@outlook.com>
2019-03-23 13:21:04 +0000

    add basic list component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import FilmList from './components/FilmList';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Film List</h1>
+        <FilmList />
       </div>
     );
   }
---
+++ client/src/components/FilmList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class FilmList extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Film 1</li>
+          <li>Film 2</li>
+        </ul>
+      </div>
+    )
+  }
+}
