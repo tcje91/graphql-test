60ec12573b29d1a02ffa4b0b5cb81d6713f45a53
tcje91 <tcje9117@outlook.com>
2019-03-24 13:03:12 +0000

    control film adder component and reset fields on submit

--- client/src/components/FilmAdder.js
+++ client/src/components/FilmAdder.js
@@ -27,6 +27,12 @@ class FilmAdder extends Component {
       },
       refetchQueries: [{ query: getFilmsQuery }]
     })
+    this.setState({
+      title: "",
+      year: "",
+      genre: "",
+      director_id: ""
+    })
   }
 
   displayDirectors() {
@@ -44,28 +50,29 @@ class FilmAdder extends Component {
     );
   }
   render() {
+    const { title, year, genre, director_id } = this.state;
     return (
       <form onSubmit={this.handleSubmit}>
         <h1>Add a film</h1>
         <div className="form-field">
           <label>Film title:</label>
-          <input type="text" name="title" onChange={this.handleInput} />
+          <input type="text" name="title" onChange={this.handleInput} value={title} />
         </div>
 
         <div className="form-field">
           <label>Year:</label>
-          <input type="text" name="year" onChange={this.handleInput} />
+          <input type="text" name="year" onChange={this.handleInput} value={year} />
         </div>
 
         <div className="form-field">
           <label>Genre:</label>
-          <input type="text" name="genre" onChange={this.handleInput} />
+          <input type="text" name="genre" onChange={this.handleInput} value={genre} />
         </div>
 
         <div className="form-field">
           <label>Director:</label>
-          <select name="director_id" onChange={this.handleInput}>
-            <option>Select director</option>
+          <select name="director_id" onChange={this.handleInput} value={director_id}>
+            <option value="">Select director</option>
             {this.displayDirectors()}
           </select>
         </div>
